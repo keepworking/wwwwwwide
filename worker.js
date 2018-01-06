e922deb0d1920ab232b6d79a17664bca9957809f
yevgeny hong <vey_robotics@naver.com>
2018-01-07 06:50:44 +0900

    update data

--- worker.js
+++ worker.js
@@ -11,17 +11,23 @@ self.addEventListener("message", function(e) {
       var baseImage = new jimp(size.width,size.height,0xFFFFFFFF)
       var backImg = target.clone();
       var frontImg = target.clone();
+      // RESIZE_NEAREST_NEIGHBOR so bad
+      // RESIZE_BILINEAR 위보단 나음
+      // RESIZE_BICUBIC bilinear 비슷
+      // RESIZE_HERMITE 비슷
+      // RESIZE_BEZIER 좀 나은것 같기도..
       backImg.resize(size.width,jimp.AUTO,jimp.RESIZE_BICUBIC)
       frontImg.resize(jimp.AUTO,size.height,jimp.RESIZE_BICUBIC)
       baseImage.composite(backImg,0,(size.height  - backImg.bitmap.height)/2)
       baseImage.blur(10)
       baseImage.composite(frontImg,(size.width -frontImg.bitmap.width)/2,0)
-      baseImage.quality(100).write(targetPath)
+      baseImage.quality(100).deflateStrategy(0).write(targetPath)
       target = null;
       backImg = null;
       frontImg = null;
       backImg = null;
       self.postMessage('finish');
+      // close();
     }).catch(function (err) {
       console.log(err)
       self.postMessage('error');
