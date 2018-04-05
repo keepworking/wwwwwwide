98482969ff4bd134c33e4d261fd99dee68360c3a
yevgeny hong <vey_robotics@naver.com>
2018-04-05 16:20:23 +0900

    가장 아랫줄 픽셀이 깨지는거 해결

--- src/worker.js
+++ src/worker.js
@@ -11,7 +11,6 @@ self.addEventListener("message", function(e) {
     console.log(e.data)
 
     jimp.read(targetPath).then(function (target) {
-      baseImage = new jimp(size.width,size.height,0xFFFFFFFF)
       backImg = target.clone();
       frontImg = target.clone();
       // RESIZE_NEAREST_NEIGHBOR so bad
@@ -19,16 +18,15 @@ self.addEventListener("message", function(e) {
       // RESIZE_BICUBIC bilinear 비슷
       // RESIZE_HERMITE 비슷
       // RESIZE_BEZIER 좀 나은것 같기도..
-      backImg.resize(size.width,jimp.AUTO,jimp.RESIZE_BICUBIC)
-      frontImg.resize(jimp.AUTO,size.height,jimp.RESIZE_BICUBIC)
-      baseImage.composite(backImg,0,(size.height  - backImg.bitmap.height)/2)
-      baseImage.blur(10)
-      baseImage.composite(frontImg,(size.width -frontImg.bitmap.width)/2,0)
-      baseImage.quality(100).deflateStrategy(0).write(targetPath)
+      backImg.resize(size.width,jimp.AUTO,jimp.RESIZE_BEZIER)
+      frontImg.resize(jimp.AUTO,size.height,jimp.RESIZE_BEZIER)
+      backImg.crop(0,(backImg.bitmap.height - size.height)/2,size.width,size.height)
+      backImg.blur(10)
+      backImg.composite(frontImg,(size.width -frontImg.bitmap.width)/2,0)
+      backImg.quality(100).deflateStrategy(0).write(targetPath)
       target = null;
       backImg = null;
       frontImg = null;
-      backImg = null;
       self.postMessage('finish');
       // close();
     }).catch(function (err) {
